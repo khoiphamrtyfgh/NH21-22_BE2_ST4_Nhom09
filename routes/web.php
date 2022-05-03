db322c69375502786c2915dd07623ab4b2511dc1
truong kien <truongkien0815@gmail.com>
2022-05-03 22:37:36 +0700

    nhom9v1.9

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -5,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\MyController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\WelcomeController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -17,13 +17,26 @@ use App\Http\Controllers\WelcomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 //Route::get('/{name?}', [WelcomeController::class, 'getAllProducts']);
 // Route::get('/{name?}', [MyController::class, 'index']);
- Route::get('/', [WelcomeController::class, 'getAllProducts']);
- //Route::get('/', [WelcomeController::class, 'getAllProduct']);
- //Route::get('/', [WelcomeController::class, 'getAllProductById']);
+Route::get('/', [WelcomeController::class, 'getAllProducts']);
+//Route::get('/', [WelcomeController::class, 'getAllProduct']);
+//Route::get('/', [WelcomeController::class, 'getAllProductById']);
 
- 
+
 
 // Route::get('/product', [MyController::class, 'product']); 
 // Route::get('/store', [MyController::class, 'store']); 
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
