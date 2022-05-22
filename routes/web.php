ffc11146a01ee67fcef7258e428ba82e404c734c
nguyenngoclananh <86284542+nguyenngoclananh@users.noreply.github.com>
2022-05-22 17:28:20 +0700

    giỏ hàng

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\MyController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProtypeController;
+use App\Helper\CartHelper;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,24 +22,38 @@ use App\Http\Controllers\WelcomeController;
 */
 
 //Route::get('/{name?}', [WelcomeController::class, 'getAllProducts']);
-// Route::get('/{name?}', [MyController::class, 'index']);
-Route::get('/', [WelcomeController::class, 'getAllProducts']);
+// Route::get('/{name?}', [MyController::class, 'index']);;
+// Route::get('/{name}', [WelcomeController::class, 'web']);
+// Route::get('/{name?}', [WelcomeController::class, 'web']);
+// Route::post('/result', [WelcomeController::class, 'getSearch']);
 //Route::get('/', [WelcomeController::class, 'getAllProduct']);
 //Route::get('/', [WelcomeController::class, 'getAllProductById']);
 
 
+// Route::get('/', [WelcomeController::class, 'getAllProducts']);
 
 // Route::get('/product', [MyController::class, 'product']); 
-// Route::get('/store', [MyController::class, 'store']); 
+// Route::post('/product', [MyController::class, 'create']); 
+// Route::get('/product', [MyController::class, 'create']); 
+Route::resource('/protype',ProtypeController::class);
+Route::resource('/product',ProductController::class);
 
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
+Route::get('/', function () {
+    return view('trangchu');
+});
+// Route::get('/dashboard', [WelcomeController::class, 'getAllProducts'])->middleware(['auth'])->name('dashboard');
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    
+     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
+Route::get('/{name?}', [WelcomeController::class, 'web']);
+Route::post('/result', [WelcomeController::class, 'getSearch']);
+//Giỏ hàng
+Route::group(['prefix' => 'cart'],function(){
+    Route::get('view',[CartController::class,'view'])->name('cart.view');
+    Route::get('add/{id}/{quantity?}',[CartController::class,'add'])->name('cart.add');
+    Route::get('remove/{id}',[CartController::class,'remove'])->name('cart.remove');
+    Route::get('update/{id}/{quantity?}',[CartController::class,'update'])->name('cart.update');
+});
+
