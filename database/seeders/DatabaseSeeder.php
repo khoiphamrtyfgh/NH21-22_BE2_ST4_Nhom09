565794d09f5674405009a34440bf4737a7023ae9
nguyenngoclananh <86284542+nguyenngoclananh@users.noreply.github.com>
2022-05-22 17:15:24 +0700

    Add files via upload

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -130,7 +130,7 @@ Cấu hình Máy tính bảng Xiaomi Pad 5 256GB\r\nMàn hình:11\",IPS LCD\r\nH
                             'manu_id' =>1,
                         'type_id' =>1,
                         'price' =>36990000,
-                        'image'=> 'iphone_13_promax.jpg',
+                        'image'=> 'iphone-13-pro-max-xanh-la-thumb-600x600.jpg',
                      'description' =>  '
                      Màn hình:  OLED6.7"Super Retina XDR
                      Hệ điều hành:iOS 15
@@ -154,7 +154,7 @@ Cấu hình Máy tính bảng Xiaomi Pad 5 256GB\r\nMàn hình:11\",IPS LCD\r\nH
                             'manu_id' =>1,
                         'type_id' =>1,
                         'price' =>16290000,
-                        'image'=> 'iphone_11_16GB_New.jpg',
+                        'image'=> 'iphone-xi-do-600x600.jpg',
                      'description' =>  '
                      Màn hình:  IPS LCD6.1"Liquid Retina
                      Hệ điều hành:iOS 15
@@ -213,7 +213,7 @@ Cấu hình Máy tính bảng Xiaomi Pad 5 256GB\r\nMàn hình:11\",IPS LCD\r\nH
             ]);
             DB::table('protypes')->insert([
                   'type_id' =>5,
-                 'type_name' =>'Đồng hồ thông minh',
+                 'type_name' =>'Đồng hồ',
             ]);
                     
              
@@ -279,6 +279,125 @@ Cấu hình Máy tính bảng Xiaomi Pad 5 256GB\r\nMàn hình:11\",IPS LCD\r\nH
                   ]);
              
                   
+
+                  DB::table('products')->insert([
+                    'id' => 11 ,
+                      'name' => 'Điện thoại Samsung Galaxy A13 6GB ',
+                          'manu_id' =>1,
+                      'type_id' =>1,
+                      'price' =>4990000,
+                      'image'=>'samsung-galaxy-a13-xanh-thumb-1-600x600.jpg',
+                   'description' =>  'Màn hình: PLS TFT LCD6.6"Full HD+
+                   Hệ điều hành:   Android 12
+                   Camera sau: Chính 50 MP & Phụ 5 MP, 2 MP, 2 MP
+                   Camera trước: 8 MP
+                   Chip: Exynos 850
+                   RAM: 6 GB  
+                   Bộ nhớ trong: 128 GB        
+                   SIM:2 Nano SIMHỗ trợ 4G
+                   Pin, Sạc:  5000 mAh15 W',
+                   'quantity' => 10,
+                   'feature' => 1,
+                   'created_at' => '2022/04/10',
+                   'updated_at' => '2022/04/10',
+                     ]);
+                 //
+                     DB::table('products')->insert([
+                        'id' => 12 ,
+                          'name' => 'iPhone 12 Pro Max 128GB ',
+                              'manu_id' =>1,
+                          'type_id' =>1,
+                          'price' =>31499000,
+                          'image'=>'iphone-12-pro-max-xanh-duong-new-600x600-600x600.jpg',
+                       'description' =>  'Cấu hình Điện thoại iPhone 12 Pro Max 128GB\r\nMàn hình: OLED, 6.7\", Super Retina XD
+                       R\r\nHệ điều hành: iOS 14\r\nCamera sau: 3 camera 12 MP\r\nCamera trước: 12 MP\r\nChip:
+                        Apple A14 Bionic\r\nRAM: 6 GB\r\nBộ nhớ trong: 128 GB\
+                       r\nSIM: 1 Nano SIM & 1 eSIM, Hỗ trợ 5G\r\nPin, Sạc: 3687 mAh, 20 W',
+                       'quantity' => 10,
+                       'feature' => 1,
+                       'created_at' => '2022/05/20',
+                       'updated_at' => '2022/05/20',
+                         ]);
+
+                         DB::table('products')->insert([
+                            'id' => 13 ,
+                              'name' => 'Laptop Asus VivoBook X515MA N4020/4GB/256GB/Win10 (EJ504T)',
+                                  'manu_id' =>5,
+                              'type_id' =>2,
+                              'price' =>8990000,
+                              'image'=>'asus-vivobook-x515ma-n4020-ej504t-600x600.jpg',
+                           'description' =>  'Cấu hình Laptop Asus VivoBook X515MA N4020/4GB/256GB/Win10 (EJ504T)\r\nCPU: Celeron, N4020, 1.1GHz\r\nRAM: 4 GB, DDR4 (1 khe), 2400 MHz\r\nỔ cứng: Hỗ trợ khe cắm HDD SATA, SSD 256 GB NVMe PCIe\r\nMàn hình: 15.6\", Full HD (1920 x 1200)\r\nCard màn hình: Card tích hợp, Intel UHD 600\r\nCổng kết nối: 1 x USB 3.2, 2 x USB 2.0, HDMI, Jack tai nghe 3.5 mm, USB Type-C\r\nHệ điều hành: Windows 10 Home SL\r\nThiết kế: Vỏ nhựa\r\nKích t
+                           hước, trọng lượng: Dài 360.2 mm - Rộng 234.9 mm - Dày 19.9 mm - Nặng 1.8 kg\r\nThời điểm ra mắt: 2020',
+                           'quantity' => 10,
+                           'feature' => 0,
+                           'created_at' => '2022/05/20',
+                           'updated_at' => '2022/05/20',
+                             ]);
+
+                             DB::table('products')->insert([
+                                'id' => 14 ,
+                                  'name' => 'Laptop Asus ZenBook UX425EA i7 1165G7/16GB/512GB/Cáp/Túi/Win10 (KI439T)',
+                                      'manu_id' =>5,
+                                  'type_id' =>2,
+                                  'price' =>30190000,
+                                  'image'=>'asus-zenbook-ux425ea-i7-ki439t-600x600.jpg',
+                               'description' =>  'Cấu hình Laptop Asus ZenBook UX425EA i7 1165G7/16GB/512GB/Cáp/Túi/Win10 (KI439T)\r\nCPU
+                               : i7, 1165G7, 2.8GHz\r\nRAM: 16 GB, LPDDR4X (On board), 4266 MHz\r\nỔ cứng: SSD 512 GB NVMe PCIe\r\n
+                               Màn hình: 14\", Full HD (1920 x 1080)\r\nCard màn hình: Card tích hợp, Intel Iris Xe\r\nCổng kết nối: 2 x T
+                               hunderbolt 4 USB-C, 1 x USB 3.2HDMI\r\nĐặc biệt: Có đèn bàn phím\r\nHệ điều hành: Windows 10 Home SL\r\nThiết kế: V
+                               ỏ kim loại nguyên khối\r\nKích thước, trọng lượng: Dài 319 mm - Rộng 208 mm - Dày 13.9 mm - Nặng 1.17 kg\r\nThời điểm 
+                               ra mắt: 2020',
+                               'quantity' => 10,
+                               'feature' => 0,
+                               'created_at' => '2022/05/20',
+                               'updated_at' => '2022/05/20',
+                                 ]);
+
+                                 DB::table('products')->insert([
+                                    'id' => 15 ,
+                                      'name' => 'Laptop Asus ZenBook UX425EA i5 1135G7/8GB/512GBCáp/Túi/Win10 (KI429T)',
+                                          'manu_id' => 5,
+                                      'type_id' => 2,
+                                      'price' => 23490000,
+                                      'image'=>'asus-zenbook-ux425ea-i5-ki429t-600x600.jpg',
+                                   'description' =>   'Cấu hình Laptop Asus ZenBook UX425EA i5 1135G7/8GB/512GBCáp/Túi/Win10 
+                                   (KI429T)\r\nCPU: i5, 1135G7, 2.4GHz\r\nRAM: 8 GB, LPDDR4X (On board), 4266 MHz\r\nỔ cứng: SSD 512 GB NVMe PCIe\r\nM
+                                   àn hình: 14\", Full HD (1920 x 1080)\r\nCard màn hình: Card tích hợp, Intel Iris Xe\r\nCổng kết nối: 1 x USB 3.2, 2 
+                                   x Thunderbolt 4 USB-C, HDMI\r\nĐặc biệt: Có đèn bàn phím\r\nHệ điều hành: Windows 10 Home SL\r\nThiết kế: Vỏ kim loại
+                                    nguyên khối\r\nKích thước, trọng lượng: Dài 319 mm - Rộng 208 mm - Dày 13.9 mm - Nặng 1.17 kg\r\nThời điểm ra mắt:
+                                    2020',
+                                   'quantity' => 10,
+                                   'feature' => 0,
+                                   'created_at' => '2022/05/20',
+                                   'updated_at' => '2022/05/20',
+                                     ]);
+
+                                     DB::table('products')->insert([
+                                        'id' => 16 ,
+                                          'name' => 'Laptop Asus TUF Gaming FX506HC i5 11400H/8GB/512GB/4GB RTX3050/144Hz/Win10 (HN002T)',
+                                              'manu_id' =>5,
+                                          'type_id' =>2,
+                                          'price' =>25690000,
+                                          'image'=>'asus-tuf-gaming-fx506hc-i5-hn002t-15-600x600.jpg',
+                                       'description' =>  'Cấu hình Laptop Asus TUF Gaming FX506HC i5 11400H/8GB/512GB/4GB RTX3050/144Hz/W
+                                       in10 (HN002T)\r\nCPU: i5, 11400H, 2.7GHz\r\nRAM: 8 GB, DDR4 2 khe (1 khe 8GB + 1 khe rời), 2933 MHz\r\nỔ cứng
+                                       : SSD 512 GB NVMe PCIe, Hỗ trợ thêm 1 khe cắm SSD M.2 PCIe mở rộng\r\nMàn hình: 15.6\", Full HD (1920 x 1080), 
+                                       144Hz\r\nCard màn hình: Card rời, RTX 3050 4GB\r\nCổng kết nối: Jack tai nghe 3.5 mm, Thunderbolt 4 USB-C,
+                                        3 x USB 3.2, HDMI, LAN (RJ45)\r\nĐặc biệt: Có đèn bàn phím\r\nHệ điều hành: Windows 10 Home SL\r\nThiết kế
+                                        : Vỏ nhựa - nắp lưng bằng kim loại\r\nKích thước, trọng lượng: Dài 359 mm - Rộng 256 mm - Dày 24.9 mm - Nặng
+                                         2.3 kg\r\nThời điểm ra mắt: 2020',
+                                       'quantity' => 10,
+                                       'feature' => 0,
+                                       'created_at' => '2022/05/20',
+                                       'updated_at' => '2022/05/20',
+                                         ]);
+
+                                         
+                                    
+                         
+ 
+
+
           
     }
 }
